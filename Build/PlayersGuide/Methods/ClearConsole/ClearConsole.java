3ce8c4d71902dee144a9420dddf15800a01e6947
Stefan <stefan.kiers89@hotmail.com>
2025-06-19 15:36:30 +0200

    number in range method

--- Build/PlayersGuide/Methods/ClearConsole/ClearConsole.java
+++
@@ -1,9 +0,0 @@
-package PlayersGuide.Methods.ClearConsole;
-
-public class ClearConsole {
-        public static void clear(){
-            for (int i = 0; i < 50; i++){
-                System.out.println();
-            }
-        }
-}
---
+++ src/main/java/org/example/Methods/ClearConsole.java
@@ -0,0 +1,9 @@
+package org.example.Methods;
+
+public class ClearConsole {
+        public static void clear(){
+            for (int i = 0; i < 50; i++){
+                System.out.println();
+            }
+        }
+}
